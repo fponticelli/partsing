498fb355a8a27ed61fc478c0bc38630706a263e9
Franco Ponticelli <franco.ponticelli@gmail.com>
2018-11-04 15:00:16 -0700

    add docs

--- lib/error/expected_eot.d.ts
+++
@@ -1,6 +0,0 @@
-import { DecodeErrorBase } from './decode_error_base';
-export declare class ExpectedEOI extends DecodeErrorBase {
-    readonly kind: 'expected-eot-error';
-    constructor();
-    toString(): string;
-}
--- lib/error/unexpected_eot.d.ts
+++
@@ -1,6 +0,0 @@
-import { DecodeErrorBase } from './decode_error_base';
-export declare class UnexpectedEOI extends DecodeErrorBase {
-    readonly kind: 'unexpected-eot-error';
-    constructor();
-    toString(): string;
-}
