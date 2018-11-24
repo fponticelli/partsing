e310b5ee7274f92484e9d567d1545e12f0f943c9
Franco Ponticelli <francop@google.com>
2018-11-24 12:22:37 -0700

    refining typedocs

---
+++ typedoc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  out: './docs',
+  name: 'ParTSing 2',
+  readme: './README.md',
+  externalPattern: 'node_modules',
+  hideGenerator: true,
+  mode: 'file',
+  module: 'commonjs',
+  theme: 'default',
+  target: 'ES5',
+  excludePrivate: true
+};
